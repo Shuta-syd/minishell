a44a96b528f68dd9c6868104b1d05e47b1762af2
Shuta-syd <shuta.ogura0921@gmail.com>
2022-06-30 23:18:18 +0900

    [add] parse.c & t_tree

--- inc/structs.h
+++ inc/structs.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   struct.h                                           :+:      :+:    :+:   */
+/*   structs.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tharaguc <tharaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 18:25:56 by shogura           #+#    #+#             */
-/*   Updated: 2022/06/30 19:14:02 by tharaguc         ###   ########.fr       */
+/*   Updated: 2022/06/30 23:12:46 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,26 @@ typedef struct s_token
 	char		*token;
 	struct s_token	*next;
 }	t_token;
+
+//抽象構文木（AST）
+typedef enum
+{
+	ND_PIPE,  // |
+	ND_SEM,	   //;
+	ND_DOLLAR, //$
+	ND_REDIRECT_IN, // >
+	ND_REDIRECT_OUT, // <
+	ND_DATA // data
+} NodeType;
+
+typedef struct s_tree
+{
+	NodeType	type;
+	char		*nd_data; //type -> ND_DATAの時だけ格納、それ以外はNULL
+	struct s_tree	*left;
+	struct s_tree	*right;
+
+} t_tree;
 
 typedef struct s_data
 {

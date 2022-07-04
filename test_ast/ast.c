a70ddf70f3e4b52c14854f71d5e25ea03c7b3c64
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-04 20:35:29 +0900

    [mod] ast.c in test_ast dir

--- test_ast/ast.c
+++ test_ast/ast.c
@@ -6,7 +6,7 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 17:25:18 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/04 20:23:52 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/04 20:34:43 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,7 +95,7 @@ t_ast *cmd(char **input){
 	while ((*input)[i] && (*input)[i] != ' ')
 		i++;
 	cmd = ft_substr(*input, 0, i);
-	*input += i + 1; // 5*6+(10+50);echo 5*(10 + 3)
+	*input += i + 1; 
 	printf("2. cmd() ->%s\n\n", *input);
 	node = node_new_val(0, cmd);
 	return node;
@@ -106,10 +106,10 @@ t_ast *expr(char **input)
 	t_ast	*node;
 
 	printf("1. expr() ->%s\n\n", *input);
-	node = mul(input);
+	node = mul(input); // 10
 	printf("2. expr() ->%s\n\n", *input);
 	if (consume(input, '+'))
-		node = node_new(ND_ADD, node, mul(input));
+		node = node_new(ND_ADD, node, mul(input)); // 50
 	printf("3. expr() ->%s\n\n", *input);
 	return node;
 }
@@ -137,10 +137,8 @@ t_ast *primary(char **input)
 	if (consume(input, '('))
 	{
 		node = expr(input);
-		if (consume(input, ')') == false)
+		if (consume(input, ')') == true)
 			return node;
-		else
-			*input++;
 	}
 	printf("2. primary() ->%s\n\n", *input);
 	num = atoi(*input);

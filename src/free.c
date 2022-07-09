9369830a1de5e239fb320b48d13e2d32c3e53120
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-09 22:41:31 +0900

    [...]

--- src/free.c
+++ src/free.c
@@ -6,24 +6,14 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 16:17:05 by tharaguc          #+#    #+#             */
-/*   Updated: 2022/07/09 19:08:26 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/09 22:41:07 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-void	mem_free(void **mem)
-{
-	free(*mem);
-	mem = NULL;
-}
-
 void	free_all(t_data *data)
 {
-	size_t i;
-
-	i = 0;
-	// print_ast(data->ast);
 	free_ast(data);
 	free_lex_lst(data);
 	if (data->input == NULL)
--- src/main.c
+++ src/main.c
@@ -6,19 +6,17 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 20:30:46 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/08 22:28:34 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/09 22:41:22 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-///*
 __attribute__((destructor))
 static void	destructor(void)
 {
 	system("leaks -q minishell");
 }
-// */
 
 
 int main(int argc, char *argv[], char **envp)
@@ -26,7 +24,6 @@ int main(int argc, char *argv[], char **envp)
 	t_data	data;
 
 	data = (t_data){};
-	data.envp = envp;
 	store_env_lst(&data, envp);
 	set_signal();
 	while (1)
@@ -37,7 +34,6 @@ int main(int argc, char *argv[], char **envp)
 		//変数展開
 		//コマンド実行
 		free_all(&data);
-		// exit(1);
 	}
 	return (0);
 }

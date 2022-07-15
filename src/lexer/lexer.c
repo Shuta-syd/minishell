8474ac450abae2d45c5d4d34b3229072c0b7f2a6
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-16 00:09:46 +0900

    [add] add comment lexer.c

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,13 +6,16 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 14:52:01 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/16 00:00:10 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/16 00:09:24 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-void	through_quote(char *input, size_t *i, char quote)
+/*
+	skip single quotes or Double quotes,
+*/
+void	skip_quote(char *input, size_t *i, char quote)
 {
 	size_t	j;
 
@@ -26,6 +29,9 @@ void	through_quote(char *input, size_t *i, char quote)
 	*i = j;
 }
 
+/*
+	Count the number of command lines
+*/
 u_int32_t	count_cmds(char *input)
 {
 	uint32_t	cmd_cnt;
@@ -36,9 +42,9 @@ u_int32_t	count_cmds(char *input)
 	while (input[i])
 	{
 		if (input[i] == '\"')
-			through_quote(input, &i, '\"');
+			skip_quote(input, &i, '\"');
 		else if (input[i] == '\'')
-			through_quote(input, &i, '\'');
+			skip_quote(input, &i, '\'');
 		else if (input[i] == '|')
 		{
 			cmd_cnt++;
@@ -49,12 +55,10 @@ u_int32_t	count_cmds(char *input)
 	return (cmd_cnt);
 }
 
-void	formatting_to_exe(t_shell *data, char *input)
-{
-
-}
-
-char **split_by_pipe(char *input, uint32_t cmd_cnt)
+/*
+	Split per pipe and extract command line (ignore pipes in double and single quarts)
+*/
+char	**split_by_pipe(char *input, uint32_t cmd_cnt)
 {
 	size_t		i_ret;
 	size_t		j;
@@ -70,7 +74,7 @@ char **split_by_pipe(char *input, uint32_t cmd_cnt)
 	while (input[j])
 	{
 		if (input[j] == '\"' || input[j] == '\'')
-			through_quote(input, &j, input[j]);
+			skip_quote(input, &j, input[j]);
 		else if (input[j] == '|')
 		{
 			ret[i_ret++] = ft_substr(input, start_input - input, &input[j] - start_input);
@@ -82,6 +86,14 @@ char **split_by_pipe(char *input, uint32_t cmd_cnt)
 	}
 	ret[i_ret] = NULL;
 	return (ret);
+}
+
+/*
+	Set up in a format that is easy to execve
+*/
+void formatting_to_exe(t_shell *data, char *input)
+{
+	
 }
 
 void lexer(t_shell *data)
@@ -98,14 +110,13 @@ void lexer(t_shell *data)
 	input = split_by_pipe(data->input, data->exe->cmd_cnt);
 	if (input == NULL)
 		exit(1);
-	// while (input[i])
-	// {
-	// 	formatting_to_exe(data, input[i]);
-	// 	i++;
-	// }
+	while (input[i])
+		printf("%s\n", input[i++]);
+	i = 0;
+	while (input[i])
+	{
+		formatting_to_exe(data, input[i]);
+		i++;
+	}
 	return ;
 }
-
-/*
-	cat < Makefile | grep all | wc -l
-*/

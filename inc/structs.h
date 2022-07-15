90ee57ad5f11fced113e1dc31b07be3eb987240b
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-15 23:56:53 +0900

    [add] split_by_pipe() in lexer.c

--- inc/structs.h
+++ inc/structs.h
@@ -6,7 +6,7 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 18:25:56 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/15 18:14:16 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/15 22:23:06 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,7 @@ typedef struct s_cmd
 */
 typedef struct s_exe
 {
-	struct s_cmd	*cmd_b;
-	struct s_cmd	*cmd_m;
-	struct s_cmd	*cmd_a;
+	struct s_cmd	*cmd;
 	char			*infile;
 	char			*outfile;
 	uint32_t		cmd_cnt;
--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,30 +6,112 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 14:52:01 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/15 18:10:00 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/15 23:56:30 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-void lexer(t_shell *shell)
+void	through_quote(char *input, size_t *i, char quote)
+{
+	size_t	j;
+
+	j = *i + 1;
+	while (input[j])
+	{
+		if (input[j] == quote)
+			break ;
+		j++;
+	}
+	*i = j;
+}
+
+u_int32_t	count_cmds(char *input)
+{
+	uint32_t	cmd_cnt;
+	size_t		i;
+
+	i = 0;
+	cmd_cnt = 1;
+	while (input[i])
+	{
+		if (input[i] == '\"')
+			through_quote(input, &i, '\"');
+		else if (input[i] == '\'')
+			through_quote(input, &i, '\'');
+		else if (input[i] == '|')
+		{
+			cmd_cnt++;
+			i++;
+		}
+		i++;
+	}
+	return (cmd_cnt);
+}
+
+void	formatting_to_exe(t_shell *data, char *input)
+{
+
+}
+
+char **split_by_pipe(char *input, uint32_t cmd_cnt)
+{
+	size_t		i_ret;
+	size_t		j;
+	char		**ret;
+	char		*start_input;
+
+	j = 0;
+	i_ret = 0;
+	start_input = input;
+	ret = ft_calloc(cmd_cnt + 1, sizeof(char *));
+	if (ret == NULL)
+		exit(1);
+	while (input[j])
+	{
+		if (input[j] == '\"')
+			through_quote(input, &j, '\"');
+		else if (input[j] == '\'')
+			through_quote(input, &j, '\'');
+		else if (input[j] == '|')
+		{
+			ret[i_ret] = ft_substr(input, start_input - input, &input[j] - start_input);
+			i_ret++;
+			start_input = input + j + 1;
+		}
+		else if (input[j + 1] == '\0')
+		{
+			ret[i_ret] = ft_substr(input, start_input - input, &input[j] - start_input + 1);
+			i_ret++;
+		}
+		j++;
+	}
+	ret[i_ret] = NULL;
+	return (ret);
+}
+
+void lexer(t_shell *data)
 {
 	size_t	i;
 	char **input;
 
 	i = 0;
-	input = ft_split(shell->input, '|');
+	data->exe = ft_calloc(1, sizeof(t_exe));
+	if (data->exe == NULL)
+		exit(1);
+	data->exe->cmd_cnt = count_cmds(data->input);
+	printf("cmd_cnt _> %u\n", data->exe->cmd_cnt);
+	input = split_by_pipe(data->input, data->exe->cmd_cnt);
 	if (input == NULL)
 		exit(1);
-	while (input[i])
-	{
-		if (ft_strchr(input[i]), )
-		{
-			/* code */
-		}
-
-		i++;
-	}
-
+	// while (input[i])
+	// {
+	// 	formatting_to_exe(data, input[i]);
+	// 	i++;
+	// }
 	return ;
 }
+
+/*
+	cat < Makefile | grep all | wc -l
+*/

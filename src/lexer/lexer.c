83d81530973867d0f2397c535c002a0bc59d4418
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-16 17:31:10 +0900

    [add] add count_arg_len()

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 14:52:01 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/16 15:02:07 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/16 17:30:30 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -168,17 +168,70 @@ void	get_env_val(t_shell *data, t_list *val, t_list *key)
 }
 
 /*
+	Count the total number of characters by expanding the environment variable name to value
+*/
+size_t	count_arg_len(char *arg, t_list *val, t_list *key)
+{
+	size_t	len;
+	t_list	*key_tmp;
+	t_list	*val_tmp;
+
+	key_tmp = key;
+	val_tmp = val;
+	len = ft_strlen(arg);
+	while (key_tmp)
+	{
+		len -= ft_strlen((char *)key_tmp->content);
+		key_tmp = key_tmp->next;
+	}
+	while (val_tmp)
+	{
+		len += ft_strlen((char *)val_tmp->content);
+		val_tmp = val_tmp->next;
+	}
+	return (len);
+}
+
+/*
+	Create environment variable expanded argument
+*/
+char	*create_expanded_arg(char *arg, t_list *val, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*ret;
+
+	i = 0;
+	j = 0;
+	ret = ft_calloc(len + 1, sizeof(char));
+	if (ret == NULL)
+		return (NULL);
+	while (arg[i])
+	{
+		if (arg[i] == '$')
+		{
+			
+		}
+		ret[j] = arg[i];
+	}
+}
+
+/*
 	Expand environment variables and create new strings
 */
 char *expand_env(char *arg, t_shell *data)
 {
+	size_t	i;
+	size_t	len;
 	t_list	env_val;
 	t_list	env_key;
 	char	*ret;
 
+	i = 0;
 	extract_env_key(arg, &env_key);
 	get_env_val(data, &env_val, &env_key);
-
+	len = count_arg_len(arg, &env_val, &env_key);
+	ret = create_expanded_arg(arg, &env_val, len);
 }
 
 /*

5851a4ba27d1eeebe881e06d2f20441755869f5f
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-21 12:28:55 +0900

    [mod]  mod extract_rediret_file()

--- src/lexer/extract_redirect.c
+++ src/lexer/extract_redirect.c
@@ -6,25 +6,28 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 11:49:55 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/21 12:17:28 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/21 12:28:04 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-char *extract_redirect_file(char *input)
+char	*extract_redirect_file(char *input)
 {
 	char	*ret_file;
 	char	*root;
 
 	ret_file = NULL;
-	root = input + 1;
+
+	input = ft_strtrim(input, " ");
+	root = input;
 	while (*input)
 	{
 		if (ft_strchr("| ", *input))
 			break ;
 		input++;
 	}
+	free(input);
 	ret_file = ft_substr(root, 0, input - root);
 	if (ret_file == NULL)
 		return (NULL);
@@ -42,10 +45,10 @@ void	store_redirect_in_out(t_shell *data, char *input)
 	while (input[i])
 	{
 		if (ft_strchr("<", input[i]))
-			data->exe->outfile = extract_redirect_file(&input[i]);
+			data->exe->outfile = extract_redirect_file(&input[i + 1]);
 		else if (ft_strchr(">", input[i]) && input[i + 1] != '>')
 		{
-			data->exe->outfile = extract_redirect_file(&input[i]);
+			data->exe->outfile = extract_redirect_file(&input[i + 1]);
 			data->exe->outfile_mode = TRUNC;
 		}
 		else if (ft_strchr(">", input[i]) && input[i + 1] != '>')
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 20:30:46 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/21 11:58:52 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/21 12:24:25 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,23 @@ __attribute__((destructor))
 static void	destructor(void)
 {
 	system("leaks -q minishell");
+}
+
+void	print_data_exe(t_shell *data)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	printf("infile ->[%s] outfile->[%s] mode->[%d]\n", data->exe->infile, data->exe->outfile, data->exe->outfile_mode);
+	printf("cmd_cnt->%zu\n", data->exe->cmd_cnt);
+	while (data->exe->cmd_cnt--)
+	{
+		j = 0;
+		while (data->exe->cmds[i].args[j])
+			printf("arg->[%s]\n", data->exe->cmds[i].args[j++]);
+		i++;
+	}
 }
 
 int main(int argc, char *argv[], char **envp)
@@ -32,6 +49,7 @@ int main(int argc, char *argv[], char **envp)
 		if (data.input == NULL)
 			exit_("\b\bexit", EXIT_SUCCESS);
 		lexer(&data);
+		print_data_exe(&data);
 		executor(&data);
 		free(data.input);
 	}

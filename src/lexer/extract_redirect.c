997bdea6271d05dde5b4414fff56e996d77c06b6
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-21 12:18:02 +0900

    [mod] extract_redirect.c

--- src/lexer/extract_redirect.c
+++ src/lexer/extract_redirect.c
@@ -6,18 +6,29 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 11:49:55 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/21 12:07:18 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/21 12:17:28 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-char *extract_outfile(char *input)
+char *extract_redirect_file(char *input)
 {
 	char	*ret_file;
+	char	*root;
 
 	ret_file = NULL;
-	
+	root = input + 1;
+	while (*input)
+	{
+		if (ft_strchr("| ", *input))
+			break ;
+		input++;
+	}
+	ret_file = ft_substr(root, 0, input - root);
+	if (ret_file == NULL)
+		return (NULL);
+	return (ret_file);
 }
 
 /*
@@ -30,8 +41,19 @@ void	store_redirect_in_out(t_shell *data, char *input)
 	i = 0;
 	while (input[i])
 	{
-		if (ft_strchr(">", input[i]))
-			data->exe->outfile = extract_outfile(&input[i]);
+		if (ft_strchr("<", input[i]))
+			data->exe->outfile = extract_redirect_file(&input[i]);
+		else if (ft_strchr(">", input[i]) && input[i + 1] != '>')
+		{
+			data->exe->outfile = extract_redirect_file(&input[i]);
+			data->exe->outfile_mode = TRUNC;
+		}
+		else if (ft_strchr(">", input[i]) && input[i + 1] != '>')
+		{
+			i++;
+			data->exe->outfile = extract_redirect_file(&input[i]);
+			data->exe->outfile_mode = APPEND;
+		}
 		i++;
 	}
 }

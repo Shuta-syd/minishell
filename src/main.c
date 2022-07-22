b9780a70a5b56883f138482be2ddf59a77c361a6
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-22 16:09:43 +0900

    [fix] lexer redirect error fixed

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 20:30:46 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/22 15:46:16 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/22 16:09:20 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,12 +59,9 @@ int main(int argc, char *argv[], char **envp)
 		data.input = readline(PROMPT);
 		if (data.input == NULL)
 			exit_("\b\bexit", EXIT_SUCCESS);
-		else if (*data.input != '\0') // empty command
-		{
-			lexer(&data);
-			print_data_exe(&data);
-			// executor(&data);
-		}
+		lexer(&data);
+		// print_data_exe(&data);
+		// executor(&data);
 		free(data.input);
 	}
 	return (0);

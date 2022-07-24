4a568d55968559a83c9c8547e85b90911348f8d3
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-24 15:24:46 +0900

    [del] del heredoc_utils.c

--- src/heredoc/heredoc.c
+++ src/heredoc/heredoc.c
@@ -6,11 +6,29 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 16:31:03 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/24 15:22:11 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/24 15:24:32 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+char	*extract_sign(char *input)
+{
+	char	*sign;
+	char	*start;
+	char	*ret;
+
+	sign = ft_strstr(input, "<<") + 2;
+	while (*sign == ' ')
+		sign++;
+	start = sign;
+	while (*sign != ' ' && *sign != '\0')
+		sign++;
+	ret = ft_substr(start, 0, sign - start);
+	if (ret == NULL)
+		return (NULL);
+	return (ret);
+}
 
 void	write_heredoc_file(t_list *heredoc_lst)
 {
--- src/heredoc/heredoc_utils.c
+++ src/heredoc/heredoc_utils.c
@@ -6,26 +6,7 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 21:36:35 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/24 15:17:09 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/24 15:24:14 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <minishell.h>
-
-char	*extract_sign(char *input)
-{
-	char	*sign;
-	char	*start;
-	char	*ret;
-
-	sign = ft_strstr(input, "<<") + 2;
-	while (*sign == ' ')
-		sign++;
-	start = sign;
-	while (*sign != ' ' && *sign != '\0')
-		sign++;
-	ret = ft_substr(start, 0, sign - start);
-	if (ret == NULL)
-		return (NULL);
-	return (ret);
-}

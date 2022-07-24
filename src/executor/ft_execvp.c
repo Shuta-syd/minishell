ba24ce5c78cb242902837f6d9d3e97cf42f8af39
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2022-07-24 16:47:38 +0900

    [add] abs execute & PATH from env list

--- src/executor/ft_execvp.c
+++ src/executor/ft_execvp.c
@@ -6,7 +6,7 @@
 /*   By: tharaguc <tharaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 22:24:20 by tharaguc          #+#    #+#             */
-/*   Updated: 2022/07/18 11:44:04 by tharaguc         ###   ########.fr       */
+/*   Updated: 2022/07/24 16:03:38 by tharaguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 static char	*get_abs_exe(char *file, char **paths);
 static void	free_dp(char **dp);
 
-int	ft_execvp(char *file, char *argv[])
+int	ft_execvp(char *file, char *argv[], t_shell *shell)
 {
 	char		**paths;
 	extern char	**environ;
 
-	paths = ft_split(getenv("PATH"), ':');
+	paths = ft_split(ms_getenv(shell, "PATH"), ':');
 	file = get_abs_exe(file, paths);
 	if (file == NULL)
 		return (-1);
@@ -35,6 +35,8 @@ static char	*get_abs_exe(char *file, char **paths)
 	char	*abs;
 	int		i;
 
+	if (access(file, X_OK) == 0)
+		return (file);
 	i = 0;
 	while (paths[i])
 	{

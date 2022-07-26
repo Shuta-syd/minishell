b10e3f442bed5ff5b0078bb5c319639f75fdd8ff
Shuta-syd <shuta.ogura0921@gmail.com>
2022-07-26 20:15:06 +0900

    [fix] main.c norm fixed

--- src/main.c
+++ src/main.c
@@ -6,42 +6,16 @@
 /*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 20:30:46 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/26 19:45:11 by shogura          ###   ########.fr       */
+/*   Updated: 2022/07/26 20:14:51 by shogura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-__attribute__((destructor))
-static void	destructor(void)
-{
-	system("leaks -q minishell");
-}
-
-// void	print_data_exe(t_shell *data)
+// __attribute__((destructor))
+// static void	destructor(void)
 // {
-// 	size_t	i;
-// 	size_t	j;
-
-// 	i = 0;
-// 	printf("infile ->[%s] outfile->[%s] mode->[%d]\n", data->exe->infile, data->exe->outfile, data->exe->outfile_mode);
-// 	printf("cmd_cnt->%zu\n", data->exe->cmd_cnt);
-// 	while (data->exe->cmd_cnt--)
-// 	{
-// 		j = 0;
-// 		while (data->exe->cmds[i].args[j])
-// 			printf("arg->[%s]\n", data->exe->cmds[i].args[j++]);
-// 		i++;
-// 	}
-// }
-
-// void	print_env_lst(t_env *env_lst)
-// {
-// 	while (env_lst)
-// 	{
-// 		printf("%s=%s\n", env_lst->key, env_lst->val);
-// 		env_lst = env_lst->next;
-// 	}
+// 	system("leaks -q minishell");
 // }
 
 static void	core(t_shell *shell);
@@ -66,7 +40,6 @@ int	main(int argc, char *argv[], char **envp)
 			core(&shell);
 		else
 			free(shell.input);
-		// printf("status > %d\n", g_status);
 	}
 	return (0);
 }

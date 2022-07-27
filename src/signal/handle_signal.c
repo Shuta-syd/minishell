86305310cf88538e6ffd5cdfdff91223d7151727
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2022-07-27 12:18:35 +0900

    [mod] ctrl-C

--- src/signal/handle_signal.c
+++ src/signal/handle_signal.c
@@ -6,7 +6,7 @@
 /*   By: tharaguc <tharaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 12:42:59 by shogura           #+#    #+#             */
-/*   Updated: 2022/07/15 14:36:42 by tharaguc         ###   ########.fr       */
+/*   Updated: 2022/07/27 12:17:26 by tharaguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ void	handle_signal(int signal)
 {
 	if (signal == SIGINT)
 	{
-		ft_putchar_fd('\n', 1);
+		ft_putstr("\n");
 		rl_on_new_line();
 		rl_redisplay();
+		g_status = 130;
 	}
 }

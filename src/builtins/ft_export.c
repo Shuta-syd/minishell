dd3330a9723add549c8536c31c99e1d8c51e0fa5
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2022-07-29 19:26:17 +0900

    [fix] export & unset

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: tharaguc <tharaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 17:38:59 by tharaguc          #+#    #+#             */
-/*   Updated: 2022/07/25 18:26:11 by tharaguc         ###   ########.fr       */
+/*   Updated: 2022/07/29 19:17:58 by tharaguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,33 @@ static bool	validate(char *arg)
 	return (true);
 }
 
+static char	*get_key(char *arg)
+{
+	size_t	i;
+	char	*res;
+
+	i = 0;
+	while (arg[i] != '=')
+		i++;
+	res = ft_calloc(i, sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	return (ft_strncpy(res, arg, i));
+}
+
 int	ft_export(t_shell *shell, char *arg)
 {
 	t_env	*new;
+	char	*key;
 
 	if (validate(arg) == false)
 	{
 		ft_putendl("not a valid identifier");
 		return (1);
 	}
+	key = get_key(arg);
+	ft_unset(shell, key);
+	free(key);
 	new = env_node_new(arg);
 	env_node_add_back(&shell->env_lst, new);
 	return (0);
